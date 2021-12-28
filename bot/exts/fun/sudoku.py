7c7bedbcca583aa60bcdc0550794155c248751bb
DMFriends <miretsky.daniel44@gmail.com>
2021-12-27 21:09:33 -0500

    More changes to Sudoku
    Made a small addition to the main function docstring, capitalized "Sudoku" where it wasn't already since it is a proper noun, added punctuation where necessary, and other grammar related changes
    Also changed the info embed variable name from `embed` to `info_embed` just for clarity (even though it is already clear what the embed is for) and added a color to the embed

--- bot/exts/fun/sudoku.py
+++ bot/exts/fun/sudoku.py
@@ -6,9 +6,9 @@ from PIL import Image, ImageDraw, ImageFont
 from discord.ext import commands
 
 from bot.bot import Bot
+from bot.constants import Colours
 
 # import random
-
 
 BACKGROUND = (242, 243, 244)
 BLACK = 0
@@ -51,49 +51,49 @@ class Sudoku(commands.Cog):
         grid. In this version of the game, however, the grid will be a 6x6 one instead of the traditional
         9x9. All numbers on the grid, traditionally, are 1-9, and no number can repeat itself in any row,
         column, or any of the smaller 3x3 grids. In this version of the game, it would be 2x3 smaller grids
-        instead of 3x3.
+        instead of 3x3 and numbers 1-6 will be used on the grid.
         """
         if not self.running:
             await self.start(ctx)
 
     @sudoku.command()
     async def start(self, ctx: commands.Context) -> None:
-        """Start a sudoku game."""
+        """Start a Sudoku game."""
         if self.running:
-            await ctx.send("A sudoku game is already running!")
+            await ctx.send("A Sudoku game is already running!")
             return
         self.running = True
         self.invoker = ctx.author
         self.started_at = datetime.datetime.now()
         # generate a sudoku board
-        await ctx.send("Started a sudoku game")
+        await ctx.send("Started a Sudoku game.")
 
     @sudoku.command(aliases=["end", "stop"])
     async def finish(self, ctx: commands.Context) -> None:
-        """End a sudoku game."""
+        """End a Sudoku game."""
         if self.running:
             if ctx.author == self.invoker:
                 self.running = False
-                await ctx.send("Ended the current game")
+                await ctx.send("Ended the current game.")
             else:
-                await ctx.send("Only the owner of the game can end the game!")
+                await ctx.send("Only the owner of the game can end it!")
         else:
-            await ctx.send("There is no sudoku game running!")
+            await ctx.send("There are no Sudoku games currently running!")
 
     @sudoku.command(aliases=["who", "information", "score"])
     async def info(self, ctx: commands.Context) -> None:
-        """Send info about the currently running sudoku game."""
+        """Send info about a currently running Sudoku game."""
         if not self.running:
-            await ctx.send("There is no running game.")
+            await ctx.send("There are no currently running games!")
             return
         now = datetime.datetime.now()
-        embed = discord.Embed(title="Sudoku Game Information")
-        embed.add_field(name="Player", value=self.invoker.name)
-        embed.add_field(name="Current time", value=(now - self.started_at))
-        embed.add_field(name="Progress", value="N/a")  # add in this variable
-        embed.add_field(name="Difficulty", value=self.difficulty)
-        embed.set_author(name=self.invoker.name, icon_url=self.invoker.display_avatar.url)
-        await ctx.send(embed=embed)
+        info_embed = discord.Embed(title="Sudoku Game Information", color=Colours.grass_green)
+        info_embed.add_field(name="Player", value=self.invoker.name)
+        info_embed.add_field(name="Current Time", value=(now - self.started_at))
+        info_embed.add_field(name="Progress", value="N/A")  # add in this variable
+        info_embed.add_field(name="Difficulty", value=self.difficulty)
+        info_embed.set_author(name=self.invoker.name, icon_url=self.invoker.display_avatar.url)
+        await ctx.send(embed=info_embed)
 
 
 def setup(bot: Bot) -> None:
